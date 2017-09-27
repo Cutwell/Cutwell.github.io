e9507964605621b6afa1721fa7037e452d7d4aa5
Zachary Smith <zacharysmith2000@googlemail.com>
2017-09-27 22:00:17 +0100

    added language banner + better background

--- static/js/git.js
+++ static/js/git.js
@@ -6,7 +6,7 @@ $.ajax({
     success: function(res) {
         var repo_html = "";
         res.map((repo) => {
-            repo_html += "<span><div class='repo-container'><div class='"+repo['language']+"-tag'></div><a class='repo-link' href='"+repo["html_url"]+"'><h1 class='repo-name'>"+repo['name']+"</h1></a><p class='repo-desc'>"+repo['description']+"</p></div></span>";
+            repo_html += "<span><div class='repo-container'><div title='Language: "+repo['language']+"' class='"+repo['language']+"-tag'></div><a class='repo-link' href='"+repo["html_url"]+"'><h1 class='repo-name'>"+repo['name']+"</h1></a><p class='repo-desc'>"+repo['description']+"</p></div></span>";
         });
         $("#repository-list").html(repo_html);
     }
