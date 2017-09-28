2280c73125639054861327f1801e8218829579a3
Zachary Smith <zacharysmith2000@googlemail.com>
2017-09-28 22:05:27 +0100

    added repo stats

--- static/js/git.js
+++ static/js/git.js
@@ -6,7 +6,8 @@ $.ajax({
     success: function(res) {
         var repo_html = "";
         res.map((repo) => {
-            repo_html += "<span><div class='repo-container'><div title='Language: "+repo['language']+"' class='"+repo['language']+"-tag'></div><a class='repo-link' href='"+repo["html_url"]+"'><h1 class='repo-name'>"+repo['name']+"</h1></a><p class='repo-desc'>"+repo['description']+"</p></div></span>";
+            console.log(repo);
+            repo_html += "<span><div class='repo-container'><div title='Language: "+repo['language']+"' class='"+repo['language']+"-tag'><p class='repo-stats'><i class='fa fa-star' aria-hidden='true'></i> "+repo['stargazers_count']+" <i class='fa fa-eye' aria-hidden='true'></i> "+repo['watchers']+" <i class='fa fa-code-fork' aria-hidden='true'></i> "+repo['forks_count']+" </p></div><a class='repo-link' href='"+repo["html_url"]+"'><h1 class='repo-name'>"+repo['name']+"</h1><p class='repo-desc'>"+repo['description']+"</p></a></div></span>";
         });
         $("#repository-list").html(repo_html);
     }
