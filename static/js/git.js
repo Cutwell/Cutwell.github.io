3e5680c0a2ba189d045cd6973641c90aa2bbffe9
Zachary Smith <zacharysmith2000@googlemail.com>
2017-09-29 22:27:54 +0100

    stopped logging objects to console

--- static/js/git.js
+++ static/js/git.js
@@ -10,7 +10,6 @@ $.ajax({
         })
         res.reverse();
         res.map((repo) => {
-            console.log(repo);
             repo_html += "<span><div class='repo-container'><div title='Language: "+repo['language']+"' class='"+repo['language']+"-tag'><p class='repo-stats'><i class='fa fa-star' aria-hidden='true'></i> "+repo['stargazers_count']+" <i class='fa fa-eye' aria-hidden='true'></i> "+repo['watchers_count']+" <i class='fa fa-code-fork' aria-hidden='true'></i> "+repo['forks_count']+" </p></div><a class='repo-link' href='"+repo["html_url"]+"'><h1 class='repo-name'>"+repo['name']+"</h1><p class='repo-desc'>"+repo['description']+"</p></a></div></span>";
         });
         $("#repository-list").html(repo_html);
