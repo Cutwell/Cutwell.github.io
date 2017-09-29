f28ed2a822b44768f88b08ba67e06208d480c46d
Zachary Smith <zacharysmith2000@googlemail.com>
2017-09-29 08:41:24 +0100

    added a sorting function to display newer repos first

--- static/js/git.js
+++ static/js/git.js
@@ -5,9 +5,13 @@ $.ajax({
     dataType: "json",
     success: function(res) {
         var repo_html = "";
+        res.sort(function(a, b) {
+            return a.id - b.id;
+        })
+        res.reverse();
         res.map((repo) => {
             console.log(repo);
-            repo_html += "<span><div class='repo-container'><div title='Language: "+repo['language']+"' class='"+repo['language']+"-tag'><p class='repo-stats'><i class='fa fa-star' aria-hidden='true'></i> "+repo['stargazers_count']+" <i class='fa fa-eye' aria-hidden='true'></i> "+repo['watchers']+" <i class='fa fa-code-fork' aria-hidden='true'></i> "+repo['forks_count']+" </p></div><a class='repo-link' href='"+repo["html_url"]+"'><h1 class='repo-name'>"+repo['name']+"</h1><p class='repo-desc'>"+repo['description']+"</p></a></div></span>";
+            repo_html += "<span><div class='repo-container'><div title='Language: "+repo['language']+"' class='"+repo['language']+"-tag'><p class='repo-stats'><i class='fa fa-star' aria-hidden='true'></i> "+repo['stargazers_count']+" <i class='fa fa-eye' aria-hidden='true'></i> "+repo['watchers_count']+" <i class='fa fa-code-fork' aria-hidden='true'></i> "+repo['forks_count']+" </p></div><a class='repo-link' href='"+repo["html_url"]+"'><h1 class='repo-name'>"+repo['name']+"</h1><p class='repo-desc'>"+repo['description']+"</p></a></div></span>";
         });
         $("#repository-list").html(repo_html);
     }
