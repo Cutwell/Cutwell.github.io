0c26bd7b4ed8c920100e9f5708f6a2b56a34bd55
Zachary Smith <zacharysmith2000@googlemail.com>
2017-10-04 13:10:00 +0100

    cleaned up

--- static/js/user.js
+++ static/js/user.js
@@ -5,7 +5,6 @@ $.ajax({
     method: "GET",
     dataType: "json",
     success: function(user) {
-        console.log(user);
         $("head").append("<title>"+user['name']+"</title><link rel='icon' type='image/png' href='"+user['avatar_url']+"'/>");
         $("header").prepend("<a href='https://github.com/"+user['login']+"'><img class='repo-container' id='avatar' src='"+user['avatar_url']+"'></a>");
         $("header").append("<div id='user-stats' class='repo-container'><i class='fa fa-book' aria-hidden='true'></i> "+user['public_repos']+" <i class='fa fa-users' aria-hidden='true'></i> "+user['followers']+" </div>");
